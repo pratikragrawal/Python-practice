c26eadc35431614d3b6f61d05a0cc99173d520aa
Pratik Agrawal <pratikagrawal787@gmail.com>
2026-06-08 21:29:03 +0530

    prac 15

---
+++ exp6.py
@@ -0,0 +1,7 @@
+name = "pratik"
+sap_id = 500123601 
+date_of_birth = "23 Oct 2005"
+address = "UPES\n\tBidholi Campus\n\tPincode : 248007"
+
+# Print the information in the specified format
+print(f"NAME : {name}\nSAP ID : {sap_id}\nDATE OF BIRTH : {date_of_birth}\nADDRESS : {address}")
