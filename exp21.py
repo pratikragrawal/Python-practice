7b9a5c55b1836bb1c9c05f51adcb5e7931a9bae9
Pratik Agrawal <pratikagrawal787@gmail.com>
2026-06-08 21:30:36 +0530

    Add files via upload

---
+++ exp21.py
@@ -0,0 +1,12 @@
+x = 9
+y = 7
+
+addition = x + y
+multiplication = x * y
+division = x / y
+subtraction = x - y
+
+print("Addition:", addition)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Subtraction:", subtraction)
