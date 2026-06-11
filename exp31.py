9938a0cd36ee73aaee841193f55edcb16fda93d3
Pratik Agrawal <pratikagrawal787@gmail.com>
2026-06-11 17:22:45 +0530

    prac 24

---
+++ exp31.py
@@ -0,0 +1,9 @@
+number = int(input("Enter any number: "))
+
+if number % 3 == 0:
+    print("It is divisible by 3")
+
+elif number % 5 == 0:
+    print("It is divisible by 5")
+    
+else: print("It is not divible by either 3 or 5 ")
