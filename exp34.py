12ed0ceedcfae9d8ca7954bf54b432189b0159c6
Pratik Agrawal <pratikagrawal787@gmail.com>
2026-06-11 17:24:06 +0530

    prac 26

---
+++ exp34.py
@@ -0,0 +1,9 @@
+x = int(input("Enter a Ist number : "))
+y = int(input("Enter a IInd number : "))
+z = int(input("Enter a third number : "))
+if(x>y and x>z):
+  print("xis greater than Y and Z")
+elif(y>x and y>z):
+ print("Y is greater than X and Z")
+else:
+  print("Z is grater than X and Y")
