a1c9c8307981a6e342bcd950439051ecb1946cdf
Pratik Agrawal <pratikagrawal787@gmail.com>
2026-06-12 23:27:44 +0530

    prac 30

---
+++ exp37.py
@@ -0,0 +1,6 @@
+date = int(input("enter a date : "))
+month = int(input("enter a month : "))
+year = int(input("Enter a year : "))
+if (date % 2 == 0):
+  print(date+1,"this is next date")
+print("O/P is : day=", date+1, "month=",month,"year=",year)
